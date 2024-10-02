463d00148aa57958c031dc17966e33f8c144953b
unknown <Jayvhonserrano@gmail.com>
2024-10-02 11:32:20 +0800

    initial commit

---
+++ helpers/Menu.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Menu Items
+ * All Project Menu
+ * @category  Menu List
+ */
+
+class Menu{
+	
+	
+			public static $navbarsideleft = array(
+		array(
+			'path' => 'home', 
+			'label' => 'Home', 
+			'icon' => '<i class="fa fa-home "></i>'
+		),
+		
+		array(
+			'path' => 'holidays', 
+			'label' => 'Holidays', 
+			'icon' => '<i class="fa fa-bell "></i>'
+		)
+	);
+		
+	
+	
+}
